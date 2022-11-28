2aa3c4e28b887d3803e2ee9b8b1f911a0623cfdd
Ahmed Mokhter <ahmedmokhttar159@gmail.com>
2022-11-29 00:55:24 +0200

    sec commit

--- src/FactoryMethod.java
+++ src/FactoryMethod.java
@@ -1,7 +1,7 @@
 public class FactoryMethod {
     public static void main(String[] args) {
         BankFactory factory=new FristBankFactory();
-        Bank bank=factory.getBank("901");
+        Bank bank=factory.getBank("9012");
         System.out.println(bank.getCash());
     }
 }
